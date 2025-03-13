529c6162cf8d6c9125504afc59944d987f06d32d
BrianHenriksen <brian.henriksen@gokstadakademiet.no>
2025-03-13 10:22:57 +0100

    Brians initial commit. Added the alfa of Battleships game logic.

--- Emne9_Prosjekt/Program.cs
+++ Emne9_Prosjekt/Program.cs
@@ -1,10 +1,12 @@
 using Emne9_Prosjekt.Components;
+using Emne9_Prosjekt.Game_components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddSingleton<BattleShipComponents>();
 
 var app = builder.Build();
 
