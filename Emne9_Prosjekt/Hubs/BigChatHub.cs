d7a341954832423c7282ea3b917ea7338b4dfcb0
Ariyelz <line_bratli@hotmail.com>
2025-04-22 11:27:56 +0200

    Treid adding usernames to bigchat

--- Emne9_Prosjekt/Hubs/BigChatHub.cs
+++ Emne9_Prosjekt/Hubs/BigChatHub.cs
@@ -1,24 +1,26 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 
+
 namespace Emne9_Prosjekt.Hubs;
 
 public class BigChatHub : Hub
 {
     public override async Task OnConnectedAsync()
     {
-        var username = Context.User?.Identity?.Name ?? "Guest";
-        await Clients.All.SendAsync("UserConnected", username);
+        
+        
+        await Clients.All.SendAsync("UserConnected");
         await base.OnConnectedAsync();
     }
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        var username = Context.User?.Identity?.Name ?? "Guest";
-        await Clients.All.SendAsync("UserDisconnected", username);
+        
+        await Clients.All.SendAsync("UserDisconnected");
         await base.OnDisconnectedAsync(exception);
     }
     public async Task SendMessage(string message)
     {
-        var username = Context.User?.Identity?.Name ?? "Guest";
-        await Clients.All.SendAsync("ReceiveMessage", username, message);
+        
+        await Clients.All.SendAsync("ReceiveMessage", message);
     }
 }
