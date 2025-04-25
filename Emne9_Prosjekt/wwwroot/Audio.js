fdaf8596c3b6dc7e2a4439345b6b07c144e9fa87
BrianHenriksen <brian.henriksen@gokstadakademiet.no>
2025-04-25 18:17:03 +0200

    Updated test for battleships page

---
+++ Emne9_Prosjekt/wwwroot/Audio.js
@@ -0,0 +1,7 @@
+﻿const soundCache = {};
+window.playSound = function (sound) {
+    if (!soundCache[sound]) {
+        soundCache[sound] = new Audio(sound);
+    }
+    soundCache[sound].play();
+};
