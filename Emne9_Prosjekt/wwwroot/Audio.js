58832db275c77a5c3a0d6b1610acd31ed72c3e9d
BrianHenriksen <brian.henriksen@gokstadakademiet.no>
2025-04-28 10:17:43 +0200

    Updated big chat, prototypes, login and all games page

--- Emne9_Prosjekt/wwwroot/Audio.js
+++ Emne9_Prosjekt/wwwroot/Audio.js
@@ -2,6 +2,7 @@
 window.playSound = function (sound) {
     if (!soundCache[sound]) {
         soundCache[sound] = new Audio(sound);
+        soundCache[sound].volume = 0.1;
     }
     soundCache[sound].play();
 };
