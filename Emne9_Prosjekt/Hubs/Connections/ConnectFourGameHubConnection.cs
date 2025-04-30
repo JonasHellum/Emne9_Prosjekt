847a635d310c4dc450bfc785d131160c7e5e585e
Ariyelz <line_bratli@hotmail.com>
2025-04-30 21:59:17 +0200

    Made interface changes for BattleShipGame connection and ConnectFourConnection, implemented interfacemethods in both razor pages.

--- Emne9_Prosjekt/Hubs/Connections/ConnectFourGameHubConnection.cs
+++ Emne9_Prosjekt/Hubs/Connections/ConnectFourGameHubConnection.cs
@@ -19,4 +19,50 @@ public class ConnectFourGameHubConnection : IConnectFourGameHubConnection
     {
         await Connection.DisposeAsync();
     }
+    // Asynkrone metoder for sending
+    public async Task SendJoinGameAsync(Dictionary<string, int> board)
+    {
+        await Connection.SendAsync("JoinGame", board);
+    }
+
+    public async Task SendGameOverAsync(bool youWon)
+    {
+        await Connection.SendAsync("GameOver", youWon);
+    }
+
+    public async Task SendUpdateBoardAsync(string opponentId, string pos)
+    {
+        await Connection.SendAsync("UpdateOpponentBoard",opponentId,pos);
+    }
+
+    public async Task SwitchTurnAsync()
+    {
+        await Connection.SendAsync("SwitchTurn");
+    }
+
+    // Registrere eventhandlers
+    public void RegisterWaitingForOpponentHandler(Func<Task> handler)
+    {
+        Connection.On("WaitingForOpponent", handler);
+    }
+
+    public void RegisterStartGameHandler(Func<Dictionary<string, int>, bool, Task> handler)
+    {
+        Connection.On("StartGame", handler);
+    }
+
+    public void RegisterUpdateBoardHandler(Func<string, Task> handler)
+    {
+        Connection.On("UpdateBoard", handler);
+    }
+
+    public void RegisterGameOverHandler(Func<bool, Task> handler)
+    {
+        Connection.On("GameOver", handler);
+    }
+
+    public void RegisterOpponentDisconnectedHandler(Func<Task> handler)
+    {
+        Connection.On("OpponentDisconnected", handler);
+    }
 }
--- Emne9_Prosjekt/Hubs/Connections/GameHubConnection.cs
+++ Emne9_Prosjekt/Hubs/Connections/GameHubConnection.cs
@@ -20,4 +20,35 @@ public class GameHubConnection : IGameHubConnection
     {
         await Connection.DisposeAsync();
     }
+    public Task SendGameOverAsync(bool youWon) => Connection.SendAsync("GameOver", youWon);
+    public async Task SendUpdateShipStatusAsync(string shipName, bool isSunk)
+    {
+        await Connection.SendAsync("UpdateShipStatus", shipName, isSunk);
+    }
+    public Task SendUpdateShotAsync(string position, string shooterId) => Connection.SendAsync("UpdateShot", position, shooterId);
+    public void RegisterStartGameHandler(Func<Dictionary<string, int>, bool, Task> handler)
+    {
+        Connection.On("StartGame", handler);
+    }
+    public void RegisterUpdateShotHandler(Func<string, string, bool, Task> handler)
+    {
+        Connection.On("UpdateShot", handler);
+    }
+    public void RegisterUpdateShipStatusHandler(Func<string, bool, Task> handler)
+    {
+        Connection.On("UpdateShipStatus", handler);
+    }
+    public void RegisterGameOverHandler(Func<bool, Task> handler)
+    {
+        Connection.On("GameOver", handler);
+    }
+    public void RegisterOpponentDisconnectedHandler(Func<Task> handler)
+    {
+        Connection.On("OpponentDisconnected", handler);
+    }
+    public void RegisterWaitingForOpponentHandler(Func<Task> handler)
+    {
+        Connection.On("WaitingForOpponent", handler);
+    }
+    public Task SendJoinGameAsync(Dictionary<string, int> board) => Connection.SendAsync("JoinGame", board);
 }
