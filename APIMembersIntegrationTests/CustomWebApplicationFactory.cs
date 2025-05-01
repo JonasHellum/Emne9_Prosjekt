15b670fa99e0abc3fdc83fcdc44a670b747fef30
Ariyelz <line_bratli@hotmail.com>
2025-05-01 07:59:14 +0200

    Added integrationTest - IntegrationTestProject for RegisterMemberController.

---
+++ APIMembersIntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using Emne9_Prosjekt.Features.Members.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace APIMembersIntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    public CustomWebApplicationFactory()
+    {
+        MemberRepositoryMock = new Mock<IMemberRepository>();
+    }
+    public Mock<IMemberRepository> MemberRepositoryMock { get; set; }
+    
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+        builder.ConfigureTestServices(services =>
+        {
+            services.AddSingleton(MemberRepositoryMock.Object);
+        });
+    }
+}
