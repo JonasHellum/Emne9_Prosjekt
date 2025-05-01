de22e3972cccd94591329123b3e2aa727a02e7b1
Ariyelz <line_bratli@hotmail.com>
2025-05-01 10:34:41 +0200

    Added unit test for BattleShip page function

---
+++ UnitTestForBattleShip/PageTests/Components/TestableGamePage.cs
@@ -0,0 +1,48 @@
+﻿using Emne9_Prosjekt.Hubs.Interfaces;
+
+namespace UnitTestForBattleShip.PageTests.Components;
+
+public class TestableGamePage
+{
+    
+    public enum GameState
+    {
+        Setup,
+        Playing,
+        GameOver
+    }
+    public GameState _gameState = GameState.Setup;
+    public Dictionary<string, int> _playerBoard = new();
+    public Dictionary<string, bool> _opponentShipStatus = new();
+    public IGameHubConnection GameHubConnection = default!;
+    public Func<Dictionary<string, List<string>>> GetPlacedShips = default!;
+
+    public string? _gameOverMessage;
+    public bool _isWinner;
+
+    public async Task CheckGameOver()
+    {
+        if (_gameState == GameState.GameOver)
+            return;
+
+        bool allMyShipsSunk = GetPlacedShips()
+            .All(ship => ship.Value.All(pos => _playerBoard.TryGetValue(pos, out var v) && v < 0));
+
+        if (allMyShipsSunk)
+        {
+            _gameState = GameState.GameOver;
+            _isWinner = false;
+            _gameOverMessage = "Du tapte! Alle dine skip er sunket.";
+            await GameHubConnection.SendGameOverAsync(true);
+            return;
+        }
+
+        if (_opponentShipStatus.Values.All(sunk => sunk) && _opponentShipStatus.Count > 0)
+        {
+            _gameState = GameState.GameOver;
+            _isWinner = true;
+            _gameOverMessage = "Du vant! Alle motstanderens skip er sunket.";
+            await GameHubConnection.SendGameOverAsync(false);
+        }
+    }
+}
