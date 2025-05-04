6d3200b06f9cca2eb45f174d569a4f4efbbf8cda
jonas Lærke Hellum <jonas_laerke@hotmail.com>
2025-05-05 00:02:05 +0200

    Added validator to leaderboardDTO stuff.

---
+++ Emne9_Prosjekt/Validators/LeaderboardValidators/LeaderboardAddOrUpdateDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using Emne9_Prosjekt.Features.Leaderboards.Models;
+using Emne9_Prosjekt.Features.Members.Models;
+using FluentValidation;
+
+namespace Emne9_Prosjekt.Validators.LeaderboardValidators;
+
+public class LeaderboardAddOrUpdateDTOValidator : AbstractValidator<LeaderboardAddOrUpdateDTO>
+{
+    public LeaderboardAddOrUpdateDTOValidator()
+    {
+        RuleFor(x => x.GameType).NotEmpty().WithMessage("GameType is required")
+            .Must(gameType => gameType == "Battleships" || gameType == "Connect 4")
+            .WithMessage("GameType must be 'Battleships' or 'Connect 4'");
+        
+        RuleFor(x => x.Losses).NotEmpty().WithMessage("Losses value is required")
+            .LessThanOrEqualTo(1).GreaterThanOrEqualTo(-1).WithMessage("Losses value must be between -1 and 1");
+        
+        RuleFor(x => x.Wins).NotEmpty().WithMessage("Losses value is required")
+            .LessThanOrEqualTo(1).GreaterThanOrEqualTo(-1).WithMessage("Losses value must be between -1 and 1");
+    }
+}
