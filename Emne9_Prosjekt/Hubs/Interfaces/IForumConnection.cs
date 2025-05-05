b5fd501cadf771683981abe69b0761b64887e961
Ariyelz <line_bratli@hotmail.com>
2025-05-05 10:40:00 +0200

    Added handlers for remaining HubConnections, + automaticreconnect

--- Emne9_Prosjekt/Hubs/Interfaces/IForumConnection.cs
+++ Emne9_Prosjekt/Hubs/Interfaces/IForumConnection.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
+﻿using Emne9_Prosjekt.Hubs.HubModels;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace Emne9_Prosjekt.Hubs.Interfaces;
 
 public interface IForumConnection : IAsyncDisposable
 {
     HubConnection Connection { get; }
+    Task SendMessageAsync(Message message);
+    Task SendCommentAsync(Comment comment);
+    void RegisterMessagesUpdatedHandler(Func<List<Message>, Task> handler);
 }
