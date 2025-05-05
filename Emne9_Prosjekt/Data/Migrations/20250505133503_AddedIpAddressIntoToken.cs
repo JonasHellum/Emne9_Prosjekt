7d5c3f06b7b9ba5b1eba26c347827be48894c2c7
jonas Lærke Hellum <jonas_laerke@hotmail.com>
2025-05-05 16:41:59 +0200

    migration for adding IP address

---
+++ Emne9_Prosjekt/Data/Migrations/20250505133503_AddedIpAddressIntoToken.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Emne9_Prosjekt.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIpAddressIntoToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IpAddress",
+                table: "MemberRefreshToken",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IpAddress",
+                table: "MemberRefreshToken");
+        }
+    }
+}
